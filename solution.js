06c478e02122e27982e7d2ccbf79bdc40128579b
Cem <cemkgrbz@gmail.com>
2022-06-30 13:26:37 +0200

    added solution.js

---
+++ solution.js
@@ -0,0 +1,13 @@
+//2
+
+console.log("Hello World")
+
+//3
+console.log("This is my first js file")
+
+// Answers
+
+// console.log "Hello World"
+
+// It doesn't print and there s an error. 
+// SyntaxError: Unexpected string
